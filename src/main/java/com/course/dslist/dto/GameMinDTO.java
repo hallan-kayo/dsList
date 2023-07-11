a3c862bfa7ba0ca961d72e56bb25db966342507c
Halan Caio <hallankayo20@gmail.com>
2023-07-10 22:52:35 -0300

    GameMinDTO, GameRepository, GameService, GamerController

---
+++ src/main/java/com/course/dslist/dto/GameMinDTO.java
@@ -0,0 +1,47 @@
+package com.course.dslist.dto;
+
+import com.course.dslist.entities.Game;
+
+public class GameMinDTO {
+
+	private Long id;
+	private String title;
+	private Integer year;
+	private String imgUrl;
+	private String shortDescription;
+	
+	public GameMinDTO() {
+		super();
+	}
+
+	public GameMinDTO(Game entity) {
+		id = entity.getId();
+		title = entity.getTitle();
+		year = entity.getYear();
+		imgUrl = entity.getImgUrl();
+		shortDescription = entity.getShortDescription();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/course/dslist/repositories/GameRepository.java
@@ -0,0 +1,13 @@
+package com.course.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.course.dslist.entities.Game;
+
+public interface GameRepository extends JpaRepository<Game, Long>{
+	
+	
+	
+	
+
+}
